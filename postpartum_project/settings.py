930287da27a30065ec8dfba3ed267cc9a4fdb4a6
pistachiogelato <168393312+pistachiogelato@users.noreply.github.com>
2024-11-04 20:14:05 +0800

    settings.py

--- postpartum_project/settings.py
+++ postpartum_project/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
