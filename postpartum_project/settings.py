bcd6c55f254f6c42d39e01863cb4a034261314aa
pistachiogelato <168393312+pistachiogelato@users.noreply.github.com>
2024-11-04 20:31:19 +0800

    settings.py

--- postpartum_project/settings.py
+++ postpartum_project/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
